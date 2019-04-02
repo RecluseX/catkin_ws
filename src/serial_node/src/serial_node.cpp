a21588cde76b99b8c8963897a6e0f6f61c3407eb
xiaoqi.yang <xiaoqi.yang@ecovacs.com>
2019-04-02 13:54:41 +0800

    add msg process

--- src/serial_node/src/serial_node.cpp
+++ src/serial_node/src/serial_node.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "serial/serial.h"
 #include <std_msgs/UInt8MultiArray.h>
+#include <std_msgs/String.h>
 
 #define MAX_BUF_LEN	128
 
@@ -12,26 +13,27 @@ public:
 	msghand(int len);
 	~msghand();
 
-	uint8_t *buf;
-	uint8_t buf_len = 0;
+	uint8_t *pBuf;
+    uint8_t buf_len;
 
+    int data_process(std_msgs::UInt8MultiArray recv_data, uint8_t recv_len);
 private:
-
+    int FrameAnalyse(uint8_t *frame, int len);
 };
 
 msghand::msghand(int len)
 {
-	buf = new uint8_t[len];
+    pBuf = new uint8_t[len];
 	buf_len = 0;
 }
 
-msghan::~msghand()
+msghand::~msghand()
 {
-	delete buf;
+	delete pBuf;
 }
 
 
-msghand *msg_t;
+msghand *pMsg;
 
 void write_callback(const std_msgs::String::ConstPtr& msg)
 {
@@ -39,75 +41,159 @@ void write_callback(const std_msgs::String::ConstPtr& msg)
         ser.write(msg->data);
 }
 
-
-int data_process(uint8_t *recv_data, uint8_t recv_len)
+int msghand::FrameAnalyse(uint8_t *frame, int len)
 {
-	if (recv_len > MAX_BUF_LEN)
-	{
-		ROS_ERROR("data overflow");
-		msg_t->buf_len = 0;
-	}
+    return 0;
 }
 
+int msghand::data_process(std_msgs::UInt8MultiArray recv_data, uint8_t recv_len)
+{
+	if (buf_len + recv_len > MAX_BUF_LEN)
+	{
+		ROS_ERROR("data overflow");
+        buf_len = 0;
+	    return 1;
+    }
+    int n = 0;
+    while (n != recv_len)
+    {
+        pBuf[buf_len + n] = recv_data.data[n];
+        n++;
+    }
+    buf_len += recv_len;
+
+    if (buf_len < 19)
+        return 2;
+
+    int32_t pos1, pos2;
+    uint8_t *pTmp;
+    bool bStartFlag = false;
+
+    int i = 0;
+    int j = 0;
+
+    pos1 = 0;
+    pos2 = 0;
+    pTmp = pBuf;
+
+    while (i < buf_len - 1)
+    {
+        if (pTmp[i] == 0xaa && pTmp[i + 1] == 0xcc)
+        {
+            pos1 = i;
+            bStartFlag = true;
+            if (i + 17 > buf_len)
+                break;
+            for (j = i + 1; j < buf_len; j++)
+            {
+                if (pTmp[j] == 0x0a && pTmp[j - 1] == 0x0d)
+                {
+                    pos2 = j;
+                    break;
+                }
+            }
+            break;
+        }
+        i++;
+    }
+
+    if (!bStartFlag)
+        return 3;
+
+    if (pos2 == 0) {
+        if (pos1 != 0) {
+            ROS_ERROR("find start but not find end");
+            memmove(pBuf, pBuf + pos1, buf_len - pos1);
+            buf_len -= pos1;
+        }
+        return 4;
+    }
+
+    if (pos2 - pos1 > 20)
+    {
+        buf_len = 0;
+        return 5;
+    }
+
+    pTmp = pBuf + pos1;
+
+    int frame_len = pos2 - pos1 + 1;
+    int ret = FrameAnalyse(pTmp, frame_len);
+
+    if (ret != 0)
+    {
+        ROS_ERROR("MCUFrameAnalyse Error:%d", ret);
+        memmove(pBuf, pBuf + pos2 + 1, buf_len - pos2 - 1);
+        buf_len -= (pos2 + 1);
+
+        return 6;
+    }
+
+    memmove(pBuf, pBuf + pos2 + 1, buf_len - pos2 - 1);
+    buf_len -= (pos2 + 1);
+
+    return 0;
+}
 
 int main(int argc, char** argv)
 {
-        ros::init(argc, argv, "my_serial");
+    ros::init(argc, argv, "my_serial");
 
-        ros::NodeHandle n;
+    ros::NodeHandle n;
 
-        ros::Subscriber write_sub = n.subscribe("write", 1000, write_callback);
-        ros::Subscriber pid_sub = n.subscribe("pid_params", 1000, pid_callback);
-        ros::Publisher  read_pub = n.advertise<std_msgs::String>("read", 1000);
-        ros::Publisher  plot_pub = n.advertise<motor_msg::motor>("motor", 1000);
-        dynamic_reconfigure::Server<my_serial::pid_paramsConfig> server;
-        dynamic_reconfigure::Server<my_serial::pid_paramsConfig>::CallbackType f;
-	
-	msg_t = new msghand(128);
-        int p, read_num = 0, recv_num = 0;
-        bool recv_head = false;
-        bool recv_tail = false;
-        uint8_t buffer[128] = {0};
+    ros::Subscriber write_sub = n.subscribe("write", 1000, write_callback);
+    ros::Publisher  read_pub = n.advertise<std_msgs::String>("read", 1000);
 
-        try {
-                ser.setPort("/dev/ttyUSB0");
-                ser.setBaudrate(115200);
-                serial::Timeout to = serial::Timeout::simpleTimeout(1000);
-                ser.setTimeout(to);
-                ser.open();
+    pMsg = new msghand(128);
+    int p, read_num = 0, recv_num = 0;
+    bool recv_head = false;
+    bool recv_tail = false;
+    uint8_t buffer[128] = {0};
+
+    try {
+            ser.setPort("/dev/ttyUSB0");
+            ser.setBaudrate(115200);
+            serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+            ser.setTimeout(to);
+            ser.open();
         }
 
-        catch (serial::IOException& e) {
-                ROS_ERROR_STREAM("Unable to open port");
-                return -1;
+    catch (serial::IOException& e) {
+            ROS_ERROR_STREAM("Unable to open port");
+            return -1;
+    }
+
+    if (ser.isOpen()) {
+            ROS_INFO_STREAM("Serial Port initialized");
+    } else {
+            return -1;
+    }
+
+
+    ros::Rate loop_rate(50);
+
+    while(ros::ok())
+    {
+        if (ser.available())
+        {
+            std_msgs::UInt8MultiArray  serial_data;
+            int i;
+            p = ser.available();
+            ser.read(serial_data.data, p);
+            int ret = pMsg->data_process(serial_data, p);
+
+            if (ret != 0)
+            {
+                ROS_ERROR("data_process error:%d", ret);
+            }
+            else
+            {
+                ROS_INFO("get frame");
+            }
         }
-
-        if (ser.isOpen()) {
-                ROS_INFO_STREAM("Serial Port initialized");
-        } else {
-                return -1;
-        }
-
-        f = boost::bind(&pid_params_callback, _1, _2);
-        server.setCallback(f);
-
-
-        ros::Rate loop_rate(50);
-
-        while(ros::ok()) {
-                if (ser.available()) {
-//                      ROS_INFO_STREAM("Reading from serial port\n");
-                        std_msgs::String result;
-                        std_msgs::UInt8MultiArray  serial_data;
-                        motor_msg::motor moto;
-                        int i;
-                        int16_t moto_data[4];
-                        p = ser.available();
-                        ser.read(serial_data.data, p);
-		}
-                ros::spinOnce();
-                loop_rate.sleep();
-        }
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 
 }
 
